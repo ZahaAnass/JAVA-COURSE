8d7a27eef5b5bb955ffb38caacd9f8a58bb89645
ZahaAnass <zahaanass277@gmail.com>
2025-10-17 21:22:23 +0100

    variable scope

---
+++ 27-variable-scope/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    static int x = 3; //CLASS
+
+    public static void main(String[] args){
+        int x = 1; //LOCAL
+        System.out.println(x);
+        doSomething();
+    }
+
+    static void doSomething(){
+        int x = 2; //LOCAL
+        System.out.println(x);
+    }
+}

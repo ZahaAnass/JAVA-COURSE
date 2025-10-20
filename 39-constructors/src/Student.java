641c3a7e334d0e8bf7e11c56687bb284c38ba3c1
ZahaAnass <zahaanass277@gmail.com>
2025-10-20 21:56:38 +0100

    student constructors

--- 39-constructors/src/Student.java
+++ 39-constructors/src/Student.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
 
 public class Student {
+
+    String name;
+    int age;
+
+    double gpa;
+
+    boolean isEnrolled;
+
+
+
+    Student(String name, int age, double gpa){
+
+        this.name = name;
+
+        this.age = age;
+
+        this.gpa = gpa;
+
+        this.isEnrolled = true;
+
+    }
+
+
+
+    void study(){
+
+        System.out.println(this.name + " is studying");
+
+    }
+
 }

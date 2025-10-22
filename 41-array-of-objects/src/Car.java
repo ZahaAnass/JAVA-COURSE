d283250d2483705d9da33b2e7643ab063e6ab0ed
ZahaAnass <zahaanass277@gmail.com>
2025-10-22 23:37:23 +0100

    adding car class

--- 41-array-of-objects/src/Car.java
+++ 41-array-of-objects/src/Car.java
@@ -1,3 +1,13 @@
 public class Car {
 
+    String brand;
+    String model;
+    int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
 }

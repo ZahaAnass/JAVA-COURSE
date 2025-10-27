f54351950c9ad7f58d05f778a2e8ae7aa1a0cc3d
ZahaAnass <zahaanass277@gmail.com>
2025-10-27 10:27:53 +0100

    write files

---
+++ 58-read-files/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // How to read a file using Java (3 popular options)
+
+        // BufferedReader + FileReader: Best for reading text files line-by-line
+        // FileInputStream: Best for binary files (e.g., images, audio files)
+        // RandomAccessFile: Best for read/write specific portions of a large file
+
+        String filePath = "../data/simple.txt";
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not locate file: " + filePath);
+        } catch (IOException e) {
+            System.out.println("Somthing went wrong reading the file");
+        }
+
+    }
+}

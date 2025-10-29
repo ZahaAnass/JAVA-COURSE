ac0e991a652077060fc4814e136c4dd5717da3f5
ZahaAnass <zahaanass277@gmail.com>
2025-10-29 18:07:30 +0100

    add music player with album selection

--- 59-music-player/src/Main.java
+++ 59-music-player/src/Main.java
@@ -8,15 +8,23 @@ public class Main {
     public static void main(String[] args) {
 
         // HOW to PLAY AUDIO with Java (.wav, .au, .aiff files)
+        // You can download free audio files from YouTube studio audio library
+        // fill the "album" folder with your audio files to test this code with allowed formats
+        // you can also convert mp3 files to wav files with online converters
 
-        String[] album = {
+        String[] albums = {
                 "Boogie-Down-The-Grey-Room-_-Golden-Palms.wav",
                 "Claim-To-Fame-The-Grey-Room-_-Clark-Sims.wav",
                 "Purple-Desire-The-Grey-Room-_-Clark-Sims.wav",
                 "Rapid-Unscheduled-Disassembly-The-Grey-Room-_-Density-_-Time.wav"
         };
 
-        String filePath = "../album/Claim-To-Fame-The-Grey-Room-_-Clark-Sims.wav";
+        String filePath = getSong(albums);
+
+        if (filePath == null) {
+            System.out.println("please select a valid song next time.");
+            return;
+        }
 
         File file = new File(filePath);
 
@@ -62,5 +70,21 @@ public class Main {
         finally {
             System.out.println("Bye !");
         }
+    }
+
+    private static String getSong(String[] albums) {
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Available songs:");
+        for (int i = 0; i < albums.length; i++) {
+            System.out.println((i + 1) + ". " + albums[i]);
+        }
+        System.out.print("Select a song to play (1-" + albums.length + "): ");
+        int choice = inputScanner.nextInt();
+        if (choice < 1 || choice > albums.length) {
+            System.out.println("Invalid choice. Exiting.");
+            return null;
+        }
+
+        return "../album/" + albums[choice - 1];
     }
 }

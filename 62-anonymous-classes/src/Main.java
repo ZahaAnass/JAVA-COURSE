8f81f9b7f054b8469e3991341a74e4082aa6cffb
ZahaAnass <zahaanass277@gmail.com>
2025-10-30 15:10:28 +0100

    anonymous-classes

---
+++ 62-anonymous-classes/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Anonymous class = A class that doesn't have a name. Cannot be reused.
+        //                   Add custom behavior without having to create a new class.
+        //                   Often used for one time uses (TimerTask, Runnable, callbacks)
+
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog() {
+            @Override
+            void speak() {
+                System.out.println("Bark");
+            }
+        };
+
+        dog1.speak(); // Outputs: Woof
+        dog2.speak(); // Outputs: Bark
+
+    }
+}

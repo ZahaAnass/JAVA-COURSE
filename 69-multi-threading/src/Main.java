10cab27e36f08fb456a048f30af65097cb79898f
ZahaAnass <zahaanass277@gmail.com>
2025-11-09 18:09:26 +0100

    add milti threading

---
+++ 69-multi-threading/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Multithreading = Enables a program to run multiple threads concurrently
+        //                  (Thread = A set of instructions that run independently)
+        //                  Useful for background tasks or time-consuming operations
+
+        Thread thread1 = new Thread(new MyRunnable("Ping"));
+        Thread thread2 = new Thread(new MyRunnable("Pong"));
+
+        System.out.println("Game Start!");
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted");
+        }
+
+        System.out.println("Game Over!");
+
+    }
+}
